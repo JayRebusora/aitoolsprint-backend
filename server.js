36e620bbc87d93b1b47212a57e6d5badb4ee371e
Jay Rebusora <67079346+JayRebusora@users.noreply.github.com>
2025-12-09 19:13:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`✅ Server running on port ${PORT}`);
-    });
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+      });
   })
   .catch((err) => {
     console.error("❌ MongoDB connection error:", err.message);
